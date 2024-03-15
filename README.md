e85742b87112bdb9166d802f88a87a92b3c6a724
Kimtaewon <kimtaewon9483@naver.com>
2024-03-15 18:24:40 +0900

    first init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # daily-study
+
+
+## 광민이형 읽어
+- https://www.peterkimzz.com/nuxt3-sideproject-2
---
+++ yyh-pj/src/test/java/com/daily/study/yyhpj/YyhPjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daily.study.yyhpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YyhPjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
